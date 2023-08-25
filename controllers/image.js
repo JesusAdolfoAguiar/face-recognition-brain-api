0d279a2a88d57bdd18d43abdb75809eb51ef52d7
Jesus Aguiar <j.adolfo.3@gmail.com>
2023-08-25 10:30:55 -0400

    code review

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,17 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: 'cd96b944584c4279a2e01e1ded9345b2'
+});
+
+const handleApiCall = (req, res) => {
+    app.models.predict('face-detection', req.body.input)
+    .then(data => {
+        res.json(data)
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+};
+
 const handleImage = (db) => (req, res) => {
     const { id } = req.body;
     db('users').where('id', '=', id)
@@ -10,5 +24,6 @@ const handleImage = (db) => (req, res) => {
   }
 
 module.exports = {
-    handleImage
+    handleImage,
+    handleApiCall
 }
